59b3e3585134c15278fe57dfee9ac790f77024b0
sssinha0 <b180043@nitsikkim.ac.in>
2025-06-17 00:47:42 +0530

    iindex change

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 const fileRoutes = require('./routes/files');
 app.use('/api/files', fileRoutes);
-app.listen(3000,'0.0.0.0', () => {
+app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
