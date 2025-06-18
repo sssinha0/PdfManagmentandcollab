7feba09264b1fe2bc02603d9c2004eaa4b09115d
sssinha0 <b180043@nitsikkim.ac.in>
2025-06-18 12:10:35 +0530

    mysql password

--- db/mysqlConfig.js
+++ db/mysqlConfig.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'root', // your MySQL password
+  password: 'Shashi@321', // your MySQL password
   database: 'pdfmanagementandcollab'
 });
 
